cfb714964a5b7053564807a26676c4927771a97d
FabianV <Fabian.Voorter@ortec-finance.com>
2019-05-10 22:58:27 +0200

    Added some fotos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,14 @@ import {Component, OnInit} from '@angular/core';
 export class HomeComponent implements OnInit {
 
   public imageSources: string[] = [
+      '../../assets/home_4.JPG',
       '../../assets/home_3.JPG',
+      '../../assets/home_8.JPG',
       '../../assets/home_5.JPG',
+      '../../assets/home_9.JPG',
       '../../assets/home_2.JPG',
-      '../../assets/home_7.JPG',
-      '../../assets/home_6.JPG'
+      '../../assets/home_10.JPG',
+      '../../assets/home_7.JPG'
   ];
 
   constructor() { }
